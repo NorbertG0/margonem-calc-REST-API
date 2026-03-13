a1a95b8194fb00ed1f97897f8c23b97a96a26ec1
Norbert <n.gajderowicz@gmail.com>
2026-03-13 12:12:54 +0100

    test: add test case for calculate attack speed

--- margo_calc/tests/item_stats_tests.py
+++ margo_calc/tests/item_stats_tests.py
@@ -81,5 +81,21 @@ def test_calculate_intellect():
     assert calculate_intellect(300, 5) == pytest.approx(837.333333)
     assert calculate_intellect(300, 6) == pytest.approx(1004)
 
+def test_calcuate_attack_speed():
+    assert calculate_attack_speed(1, -2) == pytest.approx(0.08)
+    assert calculate_attack_speed(1, -1) == pytest.approx(0.08)
+    assert calculate_attack_speed(1, 0) == 0
+    assert calculate_attack_speed(1, 1) == pytest.approx(0.08)
+    assert calculate_attack_speed(1, 2) == pytest.approx(0.08)
+    assert calculate_attack_speed(1, 3) == pytest.approx(0.09)
+    assert calculate_attack_speed(1, 4) == pytest.approx(0.09)
+    assert calculate_attack_speed(1, 5) == pytest.approx(0.09)
 
-
+    assert calculate_attack_speed(300, -2) == pytest.approx(-1.42)
+    assert calculate_attack_speed(300, -1) == pytest.approx(-0.67)
+    assert calculate_attack_speed(300, 0) == 0
+    assert calculate_attack_speed(300, 1) == pytest.approx(0.83)
+    assert calculate_attack_speed(300, 2) == pytest.approx(1.58)
+    assert calculate_attack_speed(300, 3) == pytest.approx(2.33)
+    assert calculate_attack_speed(300, 4) == pytest.approx(3.08)
+    assert calculate_attack_speed(300, 5) == pytest.approx(3.83)

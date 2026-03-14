7ba84d38c2e8aa4a38fd2db0897e24757dd3e275
Norbert <n.gajderowicz@gmail.com>
2026-03-14 19:50:00 +0100

    refactor: parametrize tests

--- margo_calc/tests/legendary_bonuses_tests.py
+++ margo_calc/tests/legendary_bonuses_tests.py
@@ -1,41 +1,75 @@
-from services.legendary_bonuses import *
+from services.legendary_bonuses import calculate_first_nerf_level, calculate_legendary_bonus_expiration, calculate_very_crit_chance, calculate_holy_touch_heal_value, calculate_anguish_damage
 import pytest
 
-def test_calculate_first_nerf_level():
-    assert calculate_first_nerf_level(200) == 250
-    assert calculate_first_nerf_level(120) == 150
-    assert calculate_first_nerf_level(119) == 149
-    assert calculate_first_nerf_level(50) == 80
+@pytest.mark.parametrize(
+    'level, expected',
+    [
+        (200, 250),
+        (120, 150),
+        (119, 149),
+        (50, 80),
+    ]
+)
 
-def test_calculate_legendary_bonus_expiration():
-    assert calculate_legendary_bonus_expiration(200) == 300
-    assert calculate_legendary_bonus_expiration(120) == 200
-    assert calculate_legendary_bonus_expiration(119) == 199
-    assert calculate_legendary_bonus_expiration(50) == 130
+def test_calculate_first_nerf_level(level, expected):
+    assert calculate_first_nerf_level(level) == expected
 
-def test_calculate_very_crit_chance():
-    assert calculate_very_crit_chance(0) == 0
-    assert calculate_very_crit_chance(10) == 17
-    assert calculate_very_crit_chance(33) == 56.1
-    assert calculate_very_crit_chance(100) == 170
+@pytest.mark.parametrize(
+    'level, expected',
+    [
+        (200, 300),
+        (120, 200),
+        (119, 199),
+        (50, 130),
+    ]
+)
 
-def test_calculate_holy_touch_heal_value():
-    assert calculate_holy_touch_heal_value(0) == 0
-    assert calculate_holy_touch_heal_value(1000) == 60
-    assert calculate_holy_touch_heal_value(10000) == 600
-    assert calculate_holy_touch_heal_value(100000) == 6000
+def test_calculate_legendary_bonus_expiration(level, expected):
+    assert calculate_legendary_bonus_expiration(level) == expected
 
-def test_calculate_anguish_damage():
-    assert calculate_anguish_damage(1, 1, 1, 1) == 0.847
-    assert calculate_anguish_damage(25, 1, 1, 1) == 1.015
-    assert calculate_anguish_damage(300, 1, 1, 1) == 2.94
+@pytest.mark.parametrize(
+    'crit_chance, expected',
+    [
+        (0, 0),
+        (10, 17),
+        (33, 56.1),
+        (100, 170),
+    ]
+)
 
-    assert calculate_anguish_damage(1, 100, 100, 100) == 84.7
-    assert calculate_anguish_damage(25, 100, 100, 100) == 101.5
-    assert calculate_anguish_damage(300, 100, 100, 100) == 294
+def test_calculate_very_crit_chance(crit_chance, expected):
+    assert calculate_very_crit_chance(crit_chance) == pytest.approx(expected)
 
-    assert calculate_anguish_damage(1, 999, 999, 999) == pytest.approx(846.15299)
-    assert calculate_anguish_damage(25, 999, 999, 999) == pytest.approx(1013.98499)
-    assert calculate_anguish_damage(300, 999, 999, 999) == 2937.06
+@pytest.mark.parametrize(
+    'hp, expected',
+    [
+        (0, 0),
+        (1000, 60),
+        (10000, 600),
+        (100000, 6000),
+    ]
+)
+
+def test_calculate_holy_touch_heal_value(hp, expected):
+    assert calculate_holy_touch_heal_value(hp) == expected
+
+@pytest.mark.parametrize(
+    'level, strength, intellect, agility, expected',
+    [
+        (1, 1, 1, 1, 0.847),
+        (25, 1, 1, 1, 1.015),
+        (300, 1, 1, 1, 2.94),
+
+        (1, 100, 100, 100, 84.7),
+        (25, 100, 100, 100, 101.5),
+        (300, 100, 100, 100, 294),
+
+        (1, 999, 999, 999, 846.15299),
+        (25, 999, 999, 999, 1013.98499),
+        (300, 999, 999, 999, 2937.06),
+    ]
+)
+def test_calculate_anguish_damage(level, strength, intellect, agility, expected):
+    assert calculate_anguish_damage(level, strength, intellect, agility) == pytest.approx(expected)
 
 
--- margo_calc/tests/strength_tests.py
+++ margo_calc/tests/strength_tests.py
@@ -1,78 +1,89 @@
 import pytest
-from services.strength import *
+from services.strength import calculate_base_hp, calculate_armor_hp, calculate_crit_value
+
+@pytest.mark.parametrize(
+    'strength, expected',
+    [
+        (100, 500),
+        (0, 0),
+        (10000, 50000),
+    ]
+)
 
 # BASE HP TESTS
-def test_calculate_base_hp_positive():
-    assert calculate_base_hp(100) == 500
+def test_calculate_base_hp(strength, expected):
+    assert calculate_base_hp(strength) == expected
 
-def test_calculate_base_hp_zero():
-    assert calculate_base_hp(0) == 0
+@pytest.mark.parametrize(
+    'strength, armor_level, expected',
+    [
+        (100, None, 0),
+        (0, None, 0),
 
-def test_calculate_base_hp_large_value():
-    assert calculate_base_hp(10000) == 50000
+        (0, 300, 0),
+        (0, 50, 0),
+        (0, 0, 0),
 
+        (100, 0, 10),
+        (50, 0, 5),
+        (100, 10, 10),
+        (100, 50, 50),
+        (100, 300, 300),
 
+        (100, 0, 10),
+        (50, 0, 5),
 
+        (100, 10, 10),
+        (100, 50, 50),
+        (100, 300, 300),
+
+        (100, 19, 20),
+        (100, 15, 20),
+        (100, 14, 10),
+        (100, 11, 10),
+    ]
+)
 # ARMOR HP GAINS TESTS
-def test_calculate_armor_hp_amor_level_none():
-    assert calculate_armor_hp(100, None) == 0
-    assert calculate_armor_hp(0, None) == 0
+def test_calculate_armor_hp(strength, armor_level, expected):
+    assert calculate_armor_hp(strength, armor_level) == expected
 
-def test_calculate_armor_hp_strength_zero():
-    assert calculate_armor_hp(0, 300) == 0
-    assert calculate_armor_hp(0, 50) == 0
-    assert calculate_armor_hp(0, 0) == 0
+@pytest.mark.parametrize(
+    'strength, level, expected',
+    [
+        (0, 20, 0),
+        (0, 15, 0),
+        (0, 9, 0),
+        (0, 0, 0),
 
-def test_calculate_armor_hp_min_multiplier():
-    assert calculate_armor_hp(100, 0) == 10
-    assert calculate_armor_hp(50, 0) == 5
+        (1, 20, 0),
+        (1, 15, 0),
+        (1, 9, 0),
+        (1, 0, 0),
 
-def test_calculate_armor_hp_normal_values():
-    assert calculate_armor_hp(100, 10) == 10
-    assert calculate_armor_hp(100, 50) == 50
-    assert calculate_armor_hp(100, 300) == 300
+        (100, 20, 0),
+        (100, 15, 0),
+        (100, 9, 0),
+        (100, 0, 0),
 
-def test_calculate_armor_hp_rounding():
-    assert calculate_armor_hp(100, 19) == 20 # 19 / 10 = 1.9 -> 2
-    assert calculate_armor_hp(100, 15) == 20 # 15 / 10 = 1.5 -> 2
-    assert calculate_armor_hp(100, 14) == 10 # 14 / 10 = 1.4 -> 1
-    assert calculate_armor_hp(100, 11) == 10 # 11 / 10 = 1.1 -> 1
+        (0, 0, 0),
+        (0, 21, 0),
+        (0, 50, 0),
+        (0, 301, 0),
 
+        (1, 21, 0.09523809),
+        (1, 50, 0.04),
+        (1, 300, 0.006666666666666667),
 
+        (100, 21, 9.523809),
+        (100, 50, 4),
+        (100, 300, 0.6666666666666666),
 
+        (1, 301, 0.006666666666666667),
+        (1, 500, 0.006666666666666667),
+        (1, 1000, 0.006666666666666667),
+    ]
+)
 # CRIT VALUE TESTS
-def test_calculate_crit_value_level_down_level_cap():
-    assert calculate_crit_value(0, 20) == 0
-    assert calculate_crit_value(0, 15) == 0
-    assert calculate_crit_value(0, 9) == 0
-    assert calculate_crit_value(0, 0) == 0
+def test_calculate_crit_value(strength, level, expected):
+    assert calculate_crit_value(strength, level) == pytest.approx(expected)
 
-    assert calculate_crit_value(1, 20) == 0
-    assert calculate_crit_value(1, 15) == 0
-    assert calculate_crit_value(1, 9) == 0
-    assert calculate_crit_value(1, 0) == 0
-
-    assert calculate_crit_value(100, 20) == 0
-    assert calculate_crit_value(100, 15) == 0
-    assert calculate_crit_value(100, 9) == 0
-    assert calculate_crit_value(100, 0) == 0
-
-def test_calculate_crit_value_strength_zero():
-    assert calculate_crit_value(0, 0) == 0
-    assert calculate_crit_value(0, 21) == 0
-    assert calculate_crit_value(0, 50) == 0
-    assert calculate_crit_value(0, 301) == 0
-
-def test_calculate_crit_value_normal_values():
-    assert calculate_crit_value(1, 21) == pytest.approx(0.09523809)
-    assert calculate_crit_value(1, 50) == pytest.approx(0.04)
-    assert calculate_crit_value(1, 300) == pytest.approx(0.006666666666666667)
-
-    assert calculate_crit_value(100, 21) == pytest.approx(9.523809)
-    assert calculate_crit_value(100, 50) == 4
-    assert calculate_crit_value(100, 300) == pytest.approx(0.6666666666666667)
-
-def test_calculate_crit_value_up_level_cap():
-    assert calculate_crit_value(1, 301) == pytest.approx(0.006666666666666667)
-    assert calculate_crit_value(1, 500) == pytest.approx(0.006666666666666667)
-    assert calculate_crit_value(1, 1000) == pytest.approx(0.006666666666666667)
